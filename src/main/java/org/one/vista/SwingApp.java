53816b3f822d8784fae6f4e8b58cd24ec27051c2
S_Uboldi <solmaruboldi@gmail.com>
2023-12-15 16:23:14 -0300

    Agregué otro panel para conversor de Temperaturas

--- src/main/java/org/one/vista/SwingApp.java
+++ src/main/java/org/one/vista/SwingApp.java
@@ -11,13 +11,15 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.text.DecimalFormat;
 
-public class SwingApp extends JFrame {
+public class SwingApp extends JFrame implements ActionListener {
 
     //Variables:
     private JTextField textCantidad;
     private JTextField textResultado;
     private JComboBox comboBoxValor;
     public double valorActual;
+    JPanel contentPane,panelPrincipal,panelBtnSeleccion, panelPantalla1,panelPantalla2, panelPantalla, panelFormMonedas, panelFormTemperaturas, panelPie;
+    JButton btnSelecMoneda, btnSelecTemperatura;
 
 
     public SwingApp() {
@@ -29,7 +31,7 @@ public class SwingApp extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Configuración JPanel Contenedor
-        JPanel contentPane = new JPanel();
+        contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setBackground(new Color(17, 0, 0));
 
@@ -37,31 +39,33 @@ public class SwingApp extends JFrame {
         contentPane.setLayout(new BorderLayout(0, 0));
 
         //Panel general principal
-        JPanel panelPrincipal = new JPanel();
+        panelPrincipal = new JPanel();
         contentPane.add(panelPrincipal, BorderLayout.CENTER);
         panelPrincipal.setLayout(new BorderLayout(0, 0));
 
         //Panel superior botones seleccionar monedas o temperaturas
-        JPanel panelBtnSeleccion = new JPanel();
+        panelBtnSeleccion = new JPanel();
         panelBtnSeleccion.setBackground(new Color(17, 0, 0));
         panelPrincipal.add(panelBtnSeleccion, BorderLayout.NORTH);
 
         //Botones de panel superior:
-        JButton btnSelecMoneda = new JButton("Monedas");
+        btnSelecMoneda = new JButton("Monedas");
         btnSelecMoneda.setForeground(new Color(255, 255, 255));
         btnSelecMoneda.setPreferredSize(new Dimension(134, 25));
         btnSelecMoneda.setBackground(new Color(192, 28, 40));
         panelBtnSeleccion.add(btnSelecMoneda);
+        btnSelecMoneda.addActionListener(this);
 
         //Agregar cuando sea necesario convertir temperaturas.
-        /*JButton btnSelecTemperatura = new JButton("Temperaturas");
+        btnSelecTemperatura = new JButton("Temperaturas");
         btnSelecTemperatura.setForeground(new Color(255, 255, 255));
         btnSelecTemperatura.setPreferredSize(new Dimension(134,25));
         btnSelecTemperatura.setBackground(new Color(192, 28, 40));
-        panelBtnSeleccion.add(btnSelecTemperatura);*/
+        panelBtnSeleccion.add(btnSelecTemperatura);
+        btnSelecTemperatura.addActionListener(this);
 
         //Panel inferior pie de App:
-        JPanel panelPie = new JPanel();
+        panelPie = new JPanel();
         panelPie.setBackground(new Color(17, 0, 0));
         panelPrincipal.add(panelPie, BorderLayout.SOUTH);
 
@@ -69,16 +73,45 @@ public class SwingApp extends JFrame {
         lblPie.setForeground(new Color(246, 245, 244));
         panelPie.add(lblPie);
 
-        //Panel BorderLayout para mostrar paneles Absolutos:
-        JPanel panelPantalla = new JPanel();
+        panelPantalla = new JPanel();
         panelPrincipal.add(panelPantalla, BorderLayout.CENTER);
-        panelPantalla.setLayout(new BorderLayout(0, 0));
+        panelPantalla.setBackground(new Color(61, 56, 70));
+        panelPantalla.setLayout(null);
 
-        //Panel formularios (absolutos):
-        JPanel panelFormMonedas = new JPanel();
+        //Panel BorderLayout para mostrar paneles Absolutos:
+        panelPantalla1 = new JPanel();
+        panelPantalla1.setBackground(new Color(61, 56, 70));
+        panelPantalla1.setBounds(10, 10, 670, 370);
+        panelPantalla1.setLayout(null);
+        panelPantalla.add(panelPantalla1);
+        cargarComponentesPantalla1();
+
+        //Panel BorderLayout para mostrar paneles Absolutos:
+        panelPantalla2 = new JPanel();
+        panelPantalla2.setBackground(Color.GREEN);
+        panelPantalla2.setBounds(10, 10, 670, 370);
+        panelPantalla2.setLayout(null);
+        panelPantalla.add(panelPantalla2);
+
+        /*//Panel convertir monedas (absolutos):
+        panelFormMonedas = new JPanel();
         panelFormMonedas.setBackground(new Color(61, 56, 70));
-        panelPantalla.add(panelFormMonedas, BorderLayout.CENTER);
-        panelFormMonedas.setLayout(null);
+        panelPantalla1.add(panelFormMonedas, BorderLayout.CENTER);
+        panelFormMonedas.setLayout(null);*/
+
+
+
+
+        //Panel convertir monedas (absolutos):
+       /* panelFormTemperaturas = new JPanel();
+        panelFormTemperaturas.setBackground(new Color(61, 56, 70));
+        panelPantalla1.add(panelFormTemperaturas, BorderLayout.CENTER);
+        panelFormTemperaturas.setLayout(null);
+        panelFormTemperaturas.setVisible(false);*/
+    }
+
+    private void cargarComponentesPantalla1() {
+
 
         // Instancia de la clase Moneda
         Moneda moneda = new Moneda();
@@ -93,38 +126,38 @@ public class SwingApp extends JFrame {
             }
         });
         comboBoxValor.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar", "ARS a USD", "USD a ARS", "ARS a EUR", "EUR a ARS"}));
-        comboBoxValor.setBounds(250, 12, 190, 30);
-        panelFormMonedas.add(comboBoxValor);
+        comboBoxValor.setBounds(245, 12, 190, 30);
+        panelPantalla1.add(comboBoxValor);
 
         //Ingresar cantidad a convertir:
         JLabel lblCantidad = new JLabel("Cantidad a convertir:");
-        lblCantidad.setForeground(new Color(255, 255, 255));
+        lblCantidad.setForeground(new Color(245, 255, 255));
         lblCantidad.setFont(new Font("Dialog", Font.BOLD, 15));
-        lblCantidad.setBounds(255, 138, 191, 25);
-        panelFormMonedas.add(lblCantidad);
+        lblCantidad.setBounds(245, 95, 191, 25);
+        panelPantalla1.add(lblCantidad);
 
         textCantidad = new JTextField();
-        textCantidad.setBounds(252, 162, 190, 30);
-        panelFormMonedas.add(textCantidad);
+        textCantidad.setBounds(245, 120, 190, 30);
+        panelPantalla1.add(textCantidad);
         textCantidad.setColumns(10);
 
         //Texto de resultado:
         JLabel lblResultado = new JLabel("Resultado:");
         lblResultado.setForeground(new Color(255, 255, 255));
         lblResultado.setFont(new Font("Dialog", Font.BOLD, 15));
-        lblResultado.setBounds(255, 234, 119, 19);
-        panelFormMonedas.add(lblResultado);
+        lblResultado.setBounds(245, 195, 119, 19);
+        panelPantalla1.add(lblResultado);
 
         textResultado = new JTextField();
         textResultado.setEditable(false);
-        textResultado.setBounds(252, 254, 190, 30);
-        panelFormMonedas.add(textResultado);
+        textResultado.setBounds(245, 220, 190, 30);
+        panelPantalla1.add(textResultado);
         textResultado.setColumns(10);
 
         //Botón convertir moneda:
         JButton btnConvertirMoneda = new JButton("Convertir");
-        btnConvertirMoneda.setBounds(250, 340, 190, 30);
-        panelFormMonedas.add(btnConvertirMoneda);
+        btnConvertirMoneda.setBounds(245, 300, 190, 30);
+        panelPantalla1.add(btnConvertirMoneda);
 
         // ActionListener para el botón de convertir
         btnConvertirMoneda.addActionListener(new ActionListener() {
@@ -147,6 +180,7 @@ public class SwingApp extends JFrame {
                 }
             }
         });
+
     }
 
     private void establecerValor() {
@@ -154,17 +188,29 @@ public class SwingApp extends JFrame {
         String mi_cambio = comboBoxValor.getSelectedItem().toString();
         switch (mi_cambio){
             case "ARS a USD":
-                valorActual = 363.03;
+                valorActual = 821.00;
                 break;
             case "USD a ARS":
-                valorActual = 0.0028;
+                valorActual = 0.0012;
                 break;
             case "ARS a EUR":
-                valorActual = 392.80;
+                valorActual = 872.87;
                 break;
             case "EUR a ARS":
-                valorActual = 0.0025;
+                valorActual = 0.0011;
                 break;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==btnSelecMoneda) {
+            panelPantalla1.setVisible(true);
+            panelPantalla2.setVisible(false);
+        }
+        if(e.getSource()==btnSelecTemperatura) {
+            panelPantalla2.setVisible(true);
+            panelPantalla1.setVisible(false);
         }
     }
 
@@ -181,5 +227,10 @@ public class SwingApp extends JFrame {
          *
          * Video para crear y configurar un JComboBox:
          * https://www.youtube.com/watch?v=3jGkv19Hg0Q
+         *
+         * Una vez funcionando la lógica del Conversor de Monedas, hay que cambiar de panel,
+         * para usar el Conversor de Temperaturas:
+         * https://www.youtube.com/watch?v=UUENqBb1l34&t=98s
+         * Para ello es muy útil seguir los pasos que explica Cristian Henao en su canal de YouTube.
          * */
 }

1e089c2085129b1356010bd475622006f032a9ea
Tuta <solmaruboldi@gmail.com>
2023-12-27 18:02:26 -0300

    Eliminé los getters y setters no usados, limpieza de código

--- src/main/java/org/one/model/Moneda.java
+++ src/main/java/org/one/model/Moneda.java
@@ -2,29 +2,15 @@ package org.one.model;
 
 public class Moneda {
     private double valor;
-    private double cantidad;
 
     private double resultado;
 
     public Moneda() {
     }
 
-    public double getValor() {
-        return valor;
-    }
-
     public double setValor(double valor) {
         this.valor = valor;
         return valor;
-    }
-
-    public double getCantidad() {
-        return cantidad;
-    }
-
-    public double setCantidad(double cantidad) {
-        this.cantidad = cantidad;
-        return cantidad;
     }
 
     public double convertirMoneda(double cantidad) {

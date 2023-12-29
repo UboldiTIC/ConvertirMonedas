d6bcca717dc598dffe17a4e101ea92a1e244568c
S_Uboldi <solmaruboldi@gmail.com>
2023-12-29 15:44:28 -0300

    Limpieza de Main y comienzo a redactar Readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ConvertirMonedas
-Proyecto en Java - Desafio del programa ONE - Oracle.
+Proyecto en Java - Desafío del programa ONE - Oracle Next Education.
+
+Este desafío fue propuesto en la formación del Programa ONE para el Grupo 4 en la orientación Backend, del cual fui parte. Este ciclo comenzó a finales de 2022 con nociones básicas de programación usando tecnologías cómo HTML y CSS, luego, se dividió en dos grandes grupos, Frontend y Backend. En la orientación Backend, durante 2023, tuvimos la oportunidad de aprender y practicar tecnologías como Java (JPA, JDBC), SQL, MySQL, Spring.
+
+Además, como se puede ver en este proyecto, utilizamos herramientas de control de versiones como Git y GitHub.
+
+Por otro lado, el proyecto permitió formar grupos de trabajo en Discord y contar con el apoyo y la colaboración de compañeros y compañeras, como así también, contribuir con otras personas que solicitaron en algún momento la colaboración para encontrar solución a algún problema. 
+
+En este sentido, también cuenta con el aporte de aprendizajes tomados de distintas fuentes de información como documentación y, fundamentalmente, de los siguientes canales de YouTube a cuyos autores se agradece especialmente y se los menciona a continuación:
+
+ // agregar aportes de youtubers
+
+Durante el trayecto se utilizaron también distintos entornos de desarrollo, Eclipse y IntellJIdea, este proyecto particularmente, fue desarrollado en IntellJIdea, pero como cuenta con la interfaz gráfica Java Swing también se utilizó Eclipse para el desarrollo gráfico, luego el código se llevó a IntellJIdea.
+
+El proyecto ConvertidorONE también cuenta con la tecnología Maven para la gestión del proyecto y sus dependencias.
+
--- src/main/java/org/one/main/Main.java
+++ src/main/java/org/one/main/Main.java
@@ -1,15 +1,6 @@
 package org.one.main;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.one.model.Cotizaciones;
-import org.one.model.Temperatura;
 import org.one.vista.SwingApp;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,77 +9,6 @@ public class Main {
         SwingApp app = new SwingApp();
         app.setVisible(true);
         app.setLocationRelativeTo(null);
-
-       /* Cotizaciones cotizaciones = new Cotizaciones();
-        cotizaciones.establecerValorBlue();
-        System.out.println(cotizaciones.getValor_blue());*/
-
-
-        /*try {
-            URL url = new URL("https://api.bluelytics.com.ar/json/last_price");
-            //Establecer conexión:
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            //¿Petición correcta?
-            int response_code = conn.getResponseCode();
-            if(response_code != 200) {
-                throw new RuntimeException("Ocurrió un error: " + response_code);
-            } else {
-                //Abrir un scanner que lee el flujo de datos recibidos:
-                StringBuilder informationString = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream());
-
-                while (scanner.hasNext()) {
-                    informationString.append(scanner.nextLine());
-                }
-                scanner.close();
-                //Mostrar la información obtenida por consola:
-                System.out.println(informationString);
-
-                *//**
-                 * Interpretar el contenido de JSON con Maven Repository org.json
-                 * En GOGODEV: https://www.youtube.com/watch?v=kSmwtbRgoDs&list=PLDllzmccetSMFnraCQgehsxfb11v8QXTP&index=2
-                 *
-                 * [] = Dentro de corchetes es un Array. Puede tener un Objeto o muchos.
-                 *      A los arreglos los llamamos JSONArray.
-                 *
-                 * {} = Dentro de llaves es un Objeto.
-                 *      A los objetos los llamamos JSONObject
-                 *
-                 * *//*
-
-                JSONArray jsonArray = new JSONArray(informationString.toString());
-                JSONObject jsonObject = jsonArray.getJSONObject(1);
-                System.out.println(jsonObject.getString("source"));
-                System.out.println(jsonObject.getDouble("value_sell"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-
-
-
-
-
-
-
-
-
-        //myTemperatura.convertirTemperatura(myTemperatura.setUnidad("celsius_fahrenheit")myTemperatura.setTemperatura(30);
-
-        /*Moneda myMoneda = new Moneda();
-        myMoneda.setTipo("USD");
-        myMoneda.setValor(355.86);
-
-
-        Cantidad myConvert = new Cantidad();
-        myConvert.setCantidad(100000);
-
-
-        System.out.println(myConvert.getCantidad() / myMoneda.getValor());*/
 
     }
 }
